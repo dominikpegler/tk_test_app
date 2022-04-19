a8fa7a524e7c9687bae57831afcc3370c10a5ba7
dominikpegler <dinkopeg@posteo.net>
2022-04-19 20:00:42 +0200

    setup file, added windows msi

--- setup.py
+++ setup.py
@@ -5,6 +5,30 @@ from cx_Freeze import setup, Executable
 # "packages": ["os"] is used as example only
 build_exe_options = {"packages": ["os"], "excludes": [], 'include_files': ['sqlite.db','icon.ico','config.json']}
 
+directory_table = [
+    ("ProgramMenuFolder", "TARGETDIR", "."),
+    ("MyProgramMenu", "ProgramMenuFolder", "MYPROG~1|My Program"),
+]
+
+msi_data = {
+    "Directory": directory_table,
+    "ProgId": [
+        ("Prog.Id", None, None, "This is a description", "IconId", None),
+    ],
+    "Icon": [
+        ("IconId", "icon.ico"),
+    ],
+}
+
+bdist_msi_options = {
+    "add_to_path": True,
+    "data": msi_data,
+    "environment_variables": [
+        ("E_MYAPP_VAR", "=-*MYAPP_VAR", "1", "TARGETDIR")
+    ],
+    "upgrade_code": "{XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}",
+}
+
 # base="Win32GUI" should be used only for Windows GUI app
 base = None
 if sys.platform == "win32":
@@ -14,6 +38,9 @@ setup(
     name="ProLite App",
     version="0.0.1",
     description="",
-    options={"build_exe": build_exe_options},
+    options={
+        "build_exe": build_exe_options,
+        "bdist_msi": bdist_msi_options
+        },
     executables=[Executable("app.py", base=base)],
 )
